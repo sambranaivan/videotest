8cd4dc1cf6a7a4d5d54697196244ded370759881
Sambrana Ivan <sambranaivan@gmail.com>
2020-02-16 21:06:57 -0300

    video y grabo

--- App.tsx
+++ App.tsx
@@ -1,28 +1,64 @@
 
 import React, { Component } from 'react';
 import { StyleSheet, Text, View , TouchableOpacity} from 'react-native';
-import { Video } from 'expo-av';
+import { Video, Audio } from 'expo-av';
+import {Camera} from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
 
 export default class App extends Component
  {
+
 
   _handleVideoRef = component => {
    
     this.setState({video1:component})
   
   }
+
+  async componentDidMount () {
+   
+    let { status } = await Permissions.askAsync(Permissions.CAMERA,Permissions.AUDIO_RECORDING, Permissions.CAMERA_ROLL)
+    
+  }
+
+ 
+  record:Promise<any> = null;
  
   
   state = { 
     isShowingText: "algo",
-    video1: null,
+    record: null,
     video2:null,
-
+    camera:null,
+    grabacion:null,
+    cameraPermission:null,
+    grabando:false
   };
 
-play = ()=>{
-  this.state.video1.playAsync();
+play = async ()=>{
+  this.setState({grabando:true})
   this.state.video2.playAsync();
+  this.record = this.state.camera.recordAsync({
+    quality:'480p'
+  });
+}
+
+stop = async () =>{
+  this.state.camera.stopRecording();
+  this.state.video2.stopAsync();
+  this.record.then(async (grabacion)=>{
+    this.setState({ grabando: false })
+    console.log(grabacion)
+    
+    const asset = await MediaLibrary.createAssetAsync(grabacion.uri);
+    if (asset) {
+      this.setState({ grabacion: null });
+    }
+
+  })
+
+  
 }
 
   render() {
@@ -30,17 +66,13 @@ play = ()=>{
      return (
       <View style={styles.container}>
             <Text>{this.state.isShowingText}</Text>
-        <Video
-           ref={this._handleVideoRef}
-          source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
-          rate={1.0}
-          volume={1.0}
-          isMuted={false}
-          resizeMode="cover"
-          // shouldPlay
-          isLooping
-          style={{ width: 300, height: 200 }}
-        />
+        <Camera 
+        ref = {ref => this.state.camera = ref}
+           style={{ width: 300, height: 200 }}
+        
+        >
+
+        </Camera>
          <Video
            ref={(ref)=>this.state.video2 = ref}
            source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
@@ -52,9 +84,20 @@ play = ()=>{
            isLooping
            style={{ width: 300, height: 200 }}
          />
-        <TouchableOpacity onPress={this.play}>
-          <Text>Play</Text>
-        </TouchableOpacity>
+      {
+        !this.state.grabando?
+           <TouchableOpacity onPress={this.play}>
+             <Text>Play</Text>
+           </TouchableOpacity>   
+        :
+      <TouchableOpacity onPress={this.stop}>
+      
+             <Text>Stop</Text>
+           </TouchableOpacity>
+      }
+         
+        
+         
       </View>
     ); 
     
